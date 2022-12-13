6cb7302def6db61d7b13adae45e4c152c2c333a9
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-13 16:19:25 +0100

    Creata struttura e raccolto valore input

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -7,4 +7,23 @@ un input.comunicagli se può partecipare o no alla festa.
 (vietato usare includes())
 */
 
-console.log("JS OK");
+//Creo lista di invitati
+
+const guest = [
+  "minelli andrea",
+  "lanci marco",
+  "lino topo",
+  "rino pape",
+  "rina pape",
+];
+
+//Targhettizzo gli elementi nella pagina
+
+const userName = document.getElementById("name");
+const button = document.getElementById("button");
+
+//Aggiungo evento al click del bottone
+
+button.addEventListener("click", function () {
+  const name = userName.value.trim().toLowerCase();
+});

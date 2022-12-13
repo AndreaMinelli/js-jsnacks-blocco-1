eb162198c4db0d39d471c740553cc875b07ea2c1
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-13 15:59:44 +0100

    Completato primo esercizio

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,3 +3,31 @@
 /*
 Il software deve chiedere per 10 volte all’utente di inserire un numero. (qui potete usare un prompt). Il programma stampa la somma di tutti i numeri inseriti.
 */
+
+//Targhettizzo gli elementi in pagina
+const totalElement = document.getElementById("total");
+
+//Creo array dove salvare i numeri
+const numbers = [];
+
+for (let i = 0; numbers.length < 10; i++) {
+  const number = parseInt(prompt("Inserisci un numero da sommare").trim());
+
+  //Controllo che i numeri inseriti siano validi
+  if (isNaN(number) || number <= 0) {
+    alert("Hai inserito un numero non valido");
+  } else {
+    numbers.push(number);
+  }
+}
+
+//Creo variabile per fare la somma
+let sum = 0;
+
+//Creo ciclo per sommare i numeri nell'array
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+
+//Stampo in pagina il risultato
+totalElement.innerText = `La somma dei tuoi numeri è ${sum}`;

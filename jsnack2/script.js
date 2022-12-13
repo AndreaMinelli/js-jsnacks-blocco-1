74e38cc2c812fb0363e64b814c0620fe97a88267
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-13 16:41:07 +0100

    Creato controllo degli invitati e stampa messaggio

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -21,9 +21,28 @@ const guest = [
 
 const userName = document.getElementById("name");
 const button = document.getElementById("button");
+const messageElement = document.getElementById("message");
 
 //Aggiungo evento al click del bottone
 
 button.addEventListener("click", function () {
-  const name = userName.value.trim().toLowerCase();
+  const name = userName.value.trim();
+  const formattedName = name.toLowerCase();
+
+  let isGuest = false;
+  let message = `Mi dispiace ${name}, non sei stato invitato, sarà per la prossima volta!`;
+
+  for (let i = 0; i < guest.length && !isGuest; i++) {
+    const guestName = guest[i];
+
+    if (guestName === formattedName) isGuest = true;
+
+    if (isGuest) {
+      message = `Salve ${name}, spero che la festa sarà di tuo gradimento!`;
+      messageElement.classList.remove("text-danger");
+      messageElement.classList.add("text-success");
+    }
+  }
+
+  messageElement.innerText = message;
 });
